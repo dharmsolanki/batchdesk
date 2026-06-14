b9b9bec69944f361363594036f8843524927f1aa
Dharm Solanki <dharmsolanki1010@gmail.com>
2026-06-14 19:49:12 +0530

    Add root index.php and .htaccess for shared hosting

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/vendor/autoload.php';
+
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+$app->bind('path.public', function() {
+    return __DIR__.'/public';
+});
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
